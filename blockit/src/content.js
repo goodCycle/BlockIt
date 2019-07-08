54f79bc7e3c3d6e5a30851f4607f2741a4633522
홍재이 <kitiyj526@gmail.com>
2019-07-08 20:04:57 +0900

    Utilize chrome message API - toggle the extensions icon can open react app

--- blockit/src/content.js
+++ blockit/src/content.js
@@ -1,4 +1,5 @@
 /* src/content.js */
+/*global chrome*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./content.css";
@@ -17,3 +18,19 @@ const app = document.createElement('div');
 app.id = "my-extension-root";
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
+
+app.style.display = "none";
+chrome.runtime.onMessage.addListener(
+   function(request, sender, sendResponse) {
+      if( request.message === "clicked_browser_action") {
+        toggle();
+      }
+   }
+);
+function toggle(){
+   if(app.style.display === "none"){
+     app.style.display = "block";
+   }else{
+     app.style.display = "none";
+   }
+}
