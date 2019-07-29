79a7fe1cb25d5a81ddddc9c4c246480a518817bb
홍재이 <kitiyj526@gmail.com>
2019-07-29 18:32:20 +0900

    [WiP] Use promise to send HTTPRequest

--- blockit/src/content.js
+++ blockit/src/content.js
@@ -4,9 +4,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Frame, { FrameContextConsumer } from 'react-frame-component';
+import { Container, Form, Button, Alert, Modal } from 'react-bootstrap';
 import "./content.css";
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      url: 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=HTDuhLv3GOM&key=AIzaSyDvh_LgkZuQxxAg2tvCN_DHKsrU8STK_jo',
+      categoryId: 'hoo'
+    };
+  }
+
+  requestCategoryId = () => {
+    // alert(JSON.parse(this.responseText));
+    var result = JSON.parse(this.responseText);
+    var categoryId = result.items[0].snippet.categoryId;
+    console.log(categoryId);
+    return categoryId;
+  }
+  
+  sendHttpRequest = (url) => {
+    // TODO: 이 코드를 프로미스를 사용해서 바꾸기
+    // https://medium.com/@kiwanjung/%EB%B2%88%EC%97%AD-async-await-%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-%EC%A0%84%EC%97%90-promise%EB%A5%BC-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-955dbac2c4a4
+    var onRequest = new XMLHttpRequest();
+    onRequest.addEventListener("load", requestCategoryId);
+    onRequest.open("GET", url);
+    onRequest.send();
+  }
+
   render() {
     return (
       <Frame head={[
@@ -22,8 +48,6 @@ class Main extends React.Component {
           // Callback is invoked with iframe's window and document instances
           ({document, window}) => {
             // Render Children
-            var url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=HTDuhLv3GOM&key=AIzaSyDvh_LgkZuQxxAg2tvCN_DHKsrU8STK_jo"
-            var categoryId = "hoo"
             return (
               <div className="my-extension">
                 <button 
@@ -66,12 +90,12 @@ function requestCategoryId () {
   return categoryId;
 }
 
-function sendHttpRequest (url) {
+sendHttpRequest (url) => {
   var onRequest = new XMLHttpRequest();
   onRequest.addEventListener("load", requestCategoryId);
   onRequest.open("GET", url);
   onRequest.send();
-}
+};
 
 function toggle(){
   if(app.style.display === "none"){
