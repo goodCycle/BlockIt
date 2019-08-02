ce8897adb37f5d1672a0e73cf02452ee32caee89
홍재이 <kitiyj526@gmail.com>
2019-08-02 23:31:41 +0900

    Use promise to getHttpRequest

--- blockit/src/content.js
+++ blockit/src/content.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Frame, { FrameContextConsumer } from 'react-frame-component';
-import { Container, Form, Button, Alert, Modal } from 'react-bootstrap';
+//import { Container, Form, Button, Alert, Modal } from 'react-bootstrap';
 import "./content.css";
 
 class Main extends React.Component {
@@ -16,21 +16,40 @@ class Main extends React.Component {
     };
   }
 
-  requestCategoryId = () => {
+  requestCategoryId = (response) => {
     // alert(JSON.parse(this.responseText));
-    var result = JSON.parse(this.responseText);
+    var result = JSON.parse(response.responseText);
     var categoryId = result.items[0].snippet.categoryId;
-    console.log(categoryId);
-    return categoryId;
+    // console.log(categoryId);
+    //return categoryId;
+    this.setState({ categoryId });
   }
   
   sendHttpRequest = (url) => {
     // TODO: 이 코드를 프로미스를 사용해서 바꾸기
     // https://medium.com/@kiwanjung/%EB%B2%88%EC%97%AD-async-await-%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-%EC%A0%84%EC%97%90-promise%EB%A5%BC-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-955dbac2c4a4
-    var onRequest = new XMLHttpRequest();
-    onRequest.addEventListener("load", requestCategoryId);
-    onRequest.open("GET", url);
-    onRequest.send();
+    return new Promise((resolve, reject) => {
+      var req = new XMLHttpRequest();
+      req.open("GET", url);
+
+      req.onload = () => {
+        if (req.status === 200) {
+          resolve(req.response);
+        }
+        else {
+          reject(Error(req.statusText));
+        }
+      }
+      // onRequest.addEventListener("load", requestCategoryId);
+      // onRequest.open("GET", url);
+
+      // Handle network errors
+      req.onerror = () => {
+        reject(Error("Network Error"));
+      }
+
+      req.send();
+    });
   }
 
   render() {
@@ -53,12 +72,14 @@ class Main extends React.Component {
                 <button 
                   onClick={() => {
                     // sendHttpRequest(url);
-                    categoryId = sendHttpRequest(url);
+                    this.sendHttpRequest(this.state.url)
+                      .then((response) => this.requestCategoryId(response))
+                      .catch((error) => alert(error));
                   }}
                 >
                   Click!
                 </button>
-                <h1>{categoryId}</h1>
+                <h1>{this.state.categoryId}</h1>
               </div>
             )}
           } 
@@ -81,21 +102,6 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
-
-function requestCategoryId () {
-  // alert(JSON.parse(this.responseText));
-  var result = JSON.parse(this.responseText);
-  var categoryId = result.items[0].snippet.categoryId;
-  console.log(categoryId);
-  return categoryId;
-}
-
-sendHttpRequest (url) => {
-  var onRequest = new XMLHttpRequest();
-  onRequest.addEventListener("load", requestCategoryId);
-  onRequest.open("GET", url);
-  onRequest.send();
-};
 
 function toggle(){
   if(app.style.display === "none"){
