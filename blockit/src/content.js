c563f86732d80bb72b3a005a1361e9232f727341
홍재이 <kitiyj526@gmail.com>
2019-08-03 00:41:36 +0900

    [WiP] Need to resolve CORS problem

--- blockit/src/content.js
+++ blockit/src/content.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom';
 import Frame, { FrameContextConsumer } from 'react-frame-component';
 //import { Container, Form, Button, Alert, Modal } from 'react-bootstrap';
 import "./content.css";
+import cors from 'cors';
 
 class Main extends React.Component {
   constructor(props) {
@@ -17,7 +18,7 @@ class Main extends React.Component {
   }
 
   requestCategoryId = (response) => {
-    // alert(JSON.parse(this.responseText));
+    console.log(response);
     var result = JSON.parse(response.responseText);
     var categoryId = result.items[0].snippet.categoryId;
     // console.log(categoryId);
@@ -25,34 +26,38 @@ class Main extends React.Component {
     this.setState({ categoryId });
   }
   
-  sendHttpRequest = (url) => {
-    // TODO: 이 코드를 프로미스를 사용해서 바꾸기
-    // https://medium.com/@kiwanjung/%EB%B2%88%EC%97%AD-async-await-%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-%EC%A0%84%EC%97%90-promise%EB%A5%BC-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-955dbac2c4a4
-    return new Promise((resolve, reject) => {
-      var req = new XMLHttpRequest();
-      req.open("GET", url);
+  // sendHttpRequest = (url) => {
+  //   // TODO: 이 코드를 프로미스를 사용해서 바꾸기
+  //   // https://medium.com/@kiwanjung/%EB%B2%88%EC%97%AD-async-await-%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-%EC%A0%84%EC%97%90-promise%EB%A5%BC-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-955dbac2c4a4
+  //   console.log('url: ', url);
+  //   return new Promise((resolve, reject) => {
+  //     var req = new XMLHttpRequest();
+  //     console.log('before GET');
+  //     req.open('GET', url);
+  //     console.log('after GET');
+  //     req.onload = () => {
+  //       if (req.status === 200) {
+  //         console.log(req.response);
+  //         resolve(req.response);
+  //       }
+  //       else {
+  //         reject(Error(req.statusText));
+  //       }
+  //     }
+  //     // req.addEventListener("load", requestCategoryId);
+  //     // onRequest.open("GET", url);
 
-      req.onload = () => {
-        if (req.status === 200) {
-          resolve(req.response);
-        }
-        else {
-          reject(Error(req.statusText));
-        }
-      }
-      // onRequest.addEventListener("load", requestCategoryId);
-      // onRequest.open("GET", url);
+  //     // Handle network errors
+  //     req.onerror = () => {
+  //       reject(Error("Network Error"));
+  //     }
 
-      // Handle network errors
-      req.onerror = () => {
-        reject(Error("Network Error"));
-      }
-
-      req.send();
-    });
-  }
+  //     req.send();
+  //   });
+  // }
 
   render() {
+    console.log('rendering...');
     return (
       <Frame head={[
         <link
@@ -71,10 +76,17 @@ class Main extends React.Component {
               <div className="my-extension">
                 <button 
                   onClick={() => {
-                    // sendHttpRequest(url);
-                    this.sendHttpRequest(this.state.url)
-                      .then((response) => this.requestCategoryId(response))
-                      .catch((error) => alert(error));
+                    console.log('before click');
+                    fetch(this.state.url)
+                      // .then((response) => {
+                      //   if (!response.ok) {
+                      //     throw Error(response.statusText);
+                      //     }
+                      //     // Read the response as json.
+                      //     return response.json();
+                      // })
+                      .then((responseAsJson) => this.requestCategoryId(responseAsJson))
+                      .catch((error) => console.log(error));
                   }}
                 >
                   Click!
@@ -90,6 +102,7 @@ class Main extends React.Component {
 }
 
 const app = document.createElement('div');
+app.use(cors());
 app.id = "my-extension-root";
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
@@ -98,6 +111,7 @@ app.style.display = "none";
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action") {
+      // alert("clicked browser!");
       toggle();
     }
   }
@@ -110,3 +124,10 @@ function toggle(){
     app.style.display = "none";
   }
 }
+
+//TODO: NOW I have to resolve CORS problem Hahah
+// https://medium.com/@nabil6391/avoid-cors-requests-for-a-react-app-2988e0061c1a
+// see above to solve the problem
+// yarn start, yarn build
+// load chrome extensions again
+// load localhost:3000 and click the icon
