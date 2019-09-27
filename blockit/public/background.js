e60046f1ebd1f5430cbd36cc707b51ea5d38a42c
홍재이 <kitiyj526@gmail.com>
2019-09-27 16:59:39 +0900

    Show modal when enter the youtube and clicks the video

--- blockit/public/background.js
+++ blockit/public/background.js
@@ -9,3 +9,9 @@ chrome.contextMenus.onClicked.addListener(() => {
     chrome.tabs.sendMessage(tabs[0].id, {type: 'getHeadlines'});
   });
 });
+
+chrome.tabs.onUpdated.addListener(() => {
+  chrome.tabs.query({active: true, currentWindow: true, status: 'complete'}, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, {type: 'urlUpdate', url: tabs[0].url});
+  });
+});
--- blockit/public/content.js
+++ blockit/public/content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(request => {
   if (request.type === 'getHeadlines') {
-    console.log('getHeadline');
+    console.log('getHeadlines here!!!')
     const modal = document.createElement('dialog');
     modal.setAttribute("style", "height:40%");
     modal.innerHTML =
@@ -19,5 +19,32 @@ chrome.runtime.onMessage.addListener(request => {
     dialog.querySelector("button").addEventListener("click", () => {
       dialog.close();
     })
+  }
+
+  if (request.type = 'urlUpdate') {
+    console.log('urlUpdate', request);
+    const url = request.url;
+    const isYoutube = url.includes('www.youtube.com');
+    const hasVideoId = url.split('v=')[1] !== undefined;
+    if (isYoutube && hasVideoId) {
+      const modal = document.createElement('dialog');
+      modal.setAttribute("style", "height:40%");
+      modal.innerHTML =
+        `<iframe id="headlineFetcher" style="height:100%"></iframe>
+          <div style="position:absolute; top:0px; left:5px;">  
+            <button>x</button>
+          </div>`;
+      document.body.appendChild(modal);
+      const dialog = document.querySelector("dialog");
+      dialog.showModal();
+      
+      const iframe = document.getElementById("headlineFetcher");
+      iframe.src = chrome.extension.getURL("index.html");
+      iframe.frameBorder = 0;
+      
+      dialog.querySelector("button").addEventListener("click", () => {
+        dialog.close();
+      })
+    }
   }
 });
