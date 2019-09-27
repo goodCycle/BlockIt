b8d6bf72d526485978d77f2a98507584d5be1631
홍재이 <kitiyj526@gmail.com>
2019-09-28 00:11:15 +0900

    Erase unnecessary codes from boilerplates

--- blockit/public/background.js
+++ blockit/public/background.js
@@ -1,13 +1,7 @@
 chrome.contextMenus.create({ 
-  id: 'HeadlineFetcher',
-  title: 'Get Headlines',
+  id: 'BlockIt',
+  title: 'BlockIt',
   contexts: ['all']
-});
-
-chrome.contextMenus.onClicked.addListener(() => {
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, {type: 'getHeadlines'});
-  });
 });
 
 chrome.tabs.onUpdated.addListener(() => {
--- blockit/public/content.js
+++ blockit/public/content.js
@@ -1,44 +1,24 @@
 chrome.runtime.onMessage.addListener(request => {
-  if (request.type === 'getHeadlines') {
-    console.log('getHeadlines here!!!')
-    const modal = document.createElement('dialog');
-    modal.setAttribute("style", "height:40%");
-    modal.innerHTML =
-      `<iframe id="headlineFetcher" style="height:100%"></iframe>
-        <div style="position:absolute; top:0px; left:5px;">  
-          <button>x</button>
-        </div>`;
-    document.body.appendChild(modal);
-    const dialog = document.querySelector("dialog");
-    dialog.showModal();
-    
-    const iframe = document.getElementById("headlineFetcher");
-    iframe.src = chrome.extension.getURL("index.html");
-    iframe.frameBorder = 0;
-    
-    dialog.querySelector("button").addEventListener("click", () => {
-      dialog.close();
-    })
-  }
-
   if (request.type = 'urlUpdate') {
-    console.log('urlUpdate', request);
     const url = request.url;
     const isYoutube = url.includes('www.youtube.com');
+    console.log('url split', url.split('v=')[1]);
     const hasVideoId = url.split('v=')[1] !== undefined;
     if (isYoutube && hasVideoId) {
       const modal = document.createElement('dialog');
-      modal.setAttribute("style", "height:40%");
+      modal.setAttribute("style", "height:60%; width: 60%");
       modal.innerHTML =
-        `<iframe id="headlineFetcher" style="height:100%"></iframe>
+        `<iframe id="BlockIt" style="height:100%"></iframe>
           <div style="position:absolute; top:0px; left:5px;">  
-            <button>x</button>
+            <button style="align-center: 'center'">x</button>
           </div>`;
       document.body.appendChild(modal);
       const dialog = document.querySelector("dialog");
-      dialog.showModal();
+      if (!dialog.open) {
+        dialog.showModal();
+      }
       
-      const iframe = document.getElementById("headlineFetcher");
+      const iframe = document.getElementById("BlockIt");
       iframe.src = chrome.extension.getURL("index.html");
       iframe.frameBorder = 0;
       
